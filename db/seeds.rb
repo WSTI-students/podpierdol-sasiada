dc4414c55becf2a7718138500f0494afa23aab9b
Magdalena <magdalena.fro@gmail.com>
2018-03-11 16:22:55 +0100

    Creation of seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create!([{email: "mail4@test.pl", first_name: "Kermit", last_name: "Frog",
+ nickname: "kermitFrog", password: "kermitkermit"},
+ {email: "mail1@test.pl", first_name: "Kulfon", last_name: "Maly",
+ nickname: "kulfonMaly", password: "kulfonkulfon"},
+ {email: "mail2@test.pl", first_name: "Muppet", last_name: "Show",
+ nickname: "muppetShow", password: "muppetmuppet"},
+ {email: "mail3@test.pl", first_name: "Maruda", last_name: "Smerf",
+ nickname: "smerfMaruda", password: "smerfsmerf"}])
