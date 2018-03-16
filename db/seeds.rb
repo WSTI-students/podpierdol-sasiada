c8cb1c1b5a65690b03abf07866d2aadfb3b01b8e
Magdalena <magdalena.fro@gmail.com>
2018-03-16 19:14:00 +0100

    Removed old hashes syntax

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-users = [{ :first_name => "Kermit", :last_name => "Frog"},
-{:first_name => "Kulfon", :last_name => "Maly"},
-{:first_name => "Muppet", :last_name => "Show"},
-{:first_name => "Maruda", :last_name => "Smerf"}]
+users = [{ first_name: "Kermit", last_name: "Frog"},
+{ first_name: "Kulfon", last_name: "Maly"},
+{ first_name: "Muppet", last_name: "Show"},
+{ first_name: "Maruda", last_name: "Smerf"}]
 
 users.each { |el| User.create!( email: "#{el[:first_name]}@mail.com",
 first_name: "#{el[:first_name]}", last_name:"#{el[:last_name]}",
