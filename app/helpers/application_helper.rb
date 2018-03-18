a2fe2fe84b403beba3ca49c2cb171824840b35d9
Magdalena <magdalena.fro@gmail.com>
2018-03-18 15:50:13 +0100

    Add helper for application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def message(flash)
+    flash.each do |type, msg|
+      if type == "notice"
+        return haml_tag :p, msg, class: "alert alert alert-success", role: "alert"
+      else type == "alert"
+        return haml_tag :p, msg, class: "alert alert alert-danger", role: "alert"
+      end
+    end
+  end
 end
