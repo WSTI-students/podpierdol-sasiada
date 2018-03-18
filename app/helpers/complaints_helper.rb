b4fd3b23404ac7607de0b819e9de6aab43341afe
sleaz0id <mat.staszczyk@gmail.com>
2018-03-18 10:49:15 +0100

    Add confirmation button helper method

--- app/helpers/complaints_helper.rb
+++ app/helpers/complaints_helper.rb
@@ -4,4 +4,9 @@ module ComplaintsHelper
     return "Potwierdź" if confirmers_count.zero?
     "Potwierdź (#{confirmers_count})"
   end
+
+  def confirm_btn_class(complaint, user)
+    return "btn btn-primary" unless user
+    complaint.confirmers.include?(user) ? "btn btn-primary" : "btn btn-outline-primary"
+  end
 end
