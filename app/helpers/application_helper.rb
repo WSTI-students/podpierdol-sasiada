b3bc84190cbef97354c1be5067f345efd60e1b10
Magdalena <magdalena.fro@gmail.com>
2018-03-23 18:24:04 +0100

    Refactor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
   def message(flash)
     flash.each do |type, msg|
       if type == "notice"
-        return haml_tag :p, msg, class: "alert alert alert-success", role: "alert"
-      else type == "alert"
-        return haml_tag :p, msg, class: "alert alert alert-danger", role: "alert"
+        haml_tag :p, msg, class: "alert alert alert-success", role: "alert"
+      else
+        haml_tag :p, msg, class: "alert alert alert-danger", role: "alert"
       end
     end
   end
